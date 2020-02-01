6da509624294ca146661c586e2ffe106b542b3cc
Lucas Di Fini <difinile@gmail.com>
2020-02-01 13:37:16 -0700

    Updates

---
+++ precache-manifest.144d81b597cd3a5b7573b6b01c964c02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33929f9462fef468b748713058dc493a",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "b5f93cdf4df9ddf5c2c8",
+    "url": "/ClickyGame/static/css/main.2bef2f66.chunk.css"
+  },
+  {
+    "revision": "211f0bf1b5536bb62b4f",
+    "url": "/ClickyGame/static/js/2.2c8dfdfb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.2c8dfdfb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5f93cdf4df9ddf5c2c8",
+    "url": "/ClickyGame/static/js/main.e6b0c758.chunk.js"
+  },
+  {
+    "revision": "6b50fec77d0993c6099f",
+    "url": "/ClickyGame/static/js/runtime-main.410d74f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
